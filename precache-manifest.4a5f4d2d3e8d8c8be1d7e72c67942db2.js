441477e3e88fda6670d60e268136d7e8600e10f9
ldrummondbruce <52721682+ldrummondbruce@users.noreply.github.com>
2020-04-12 18:40:25 -0400

    Updates

---
+++ precache-manifest.4a5f4d2d3e8d8c8be1d7e72c67942db2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d298f76f7adc8a3726c39d6a847dc773",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "fbd1ddd2d3351c546b98",
+    "url": "/monsters-rolodex/static/css/main.0ef444af.chunk.css"
+  },
+  {
+    "revision": "926634218f0cd358d7da",
+    "url": "/monsters-rolodex/static/js/2.a6bff23a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.a6bff23a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fbd1ddd2d3351c546b98",
+    "url": "/monsters-rolodex/static/js/main.ad8d2daf.chunk.js"
+  },
+  {
+    "revision": "fc1a59712d7f10d18ab0",
+    "url": "/monsters-rolodex/static/js/runtime-main.664c0a0d.js"
+  }
+]);
